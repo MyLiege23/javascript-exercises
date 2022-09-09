d91bbbbe48ab983285809e2aed2dbbb6c4f1b791
MyLiege23 <hsabel@outlook.com>
2022-09-09 17:28:54 -0600

    Complete 02_repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+  let output = "";
+  if (n >= 0) {
+    for (let i = 0; i < n; i++) {
+    output += str;
+    }
+    return output;
+  }
+  return "ERROR";
 };
 
 // Do not edit below this line
