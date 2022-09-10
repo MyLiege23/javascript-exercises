14d84ca20a11283c954882376b12025f080c9001
MyLiege23 <hsabel@outlook.com>
2022-09-09 19:04:47 -0600

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(intStart, intEnd) {
+  let sum = 0;
+  if (typeof(intStart) !== "number" || typeof(intEnd) !== "number" || intStart < 0 || intEnd < 0) {
+    return "ERROR";
+  } else if (intStart < intEnd) {
+    for (let i = intStart; i <= intEnd; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = intEnd; i<= intStart; i++) {
+      sum += i;
+    }}
+  return sum;
 };
+
+console.log(sumAll(10, "90"));
 
 // Do not edit below this line
 module.exports = sumAll;
