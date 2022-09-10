2297e311f64747d26b5acd1659b124dfdf9e56b1
MyLiege23 <hsabel@outlook.com>
2022-09-09 18:27:08 -0600

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+  for (let a = 0; a < arguments.length; a++) {
+    console.log(arguments[a]);
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === arguments[a]) {
+        arr.splice(i, 1);
+        console.log(arr);
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
