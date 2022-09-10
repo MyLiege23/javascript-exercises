35892179f7d85402822dc218ffb9feb7d1d5c2cb
MyLiege23 <hsabel@outlook.com>
2022-09-09 20:12:39 -0600

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,48 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (let a = 0; a < arr.length; a++) {
+    sum += arr[a];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for (let a = 0; a < arr.length; a++) {
+    product *= arr[a];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(n, exponent) {
+  let product = n;
+  for (let e = 2; e <= exponent; e++) {
+    product *= n;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(n) {
+	let product = 1;
+  for (f = n; n > 0; n--) {
+    product *= n; 
+  }
+  return product;
+}
+console.log(add(2, 3))
+console.log(subtract(4, 1))
+console.log(sum([5, 4, 6]))
+console.log(multiply([5, 2, 1, 3]))
+console.log(power(2, 4))
+console.log(factorial(4))
 
 // Do not edit below this line
 module.exports = {
