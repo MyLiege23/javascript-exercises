366b521647bb543b6d33d59d1817e43a2dc1118e
MyLiege23 <hsabel@outlook.com>
2022-09-09 19:57:38 -0600

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return Math.round((temp - 32) * 5 / 9 * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return Math.round((temp * 9 / 5 + 32) * 10) / 10;
 };
+
+console.log(ftoc(72));
+console.log(ctof(73.2));
 
 // Do not edit below this line
 module.exports = {
