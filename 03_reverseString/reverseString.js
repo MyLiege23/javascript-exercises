8c7b10e39ac12f49cd3733cbecce14b935bb7c43
MyLiege23 <hsabel@outlook.com>
2022-09-09 18:04:23 -0600

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let backwardsArr = [];
+  let backwardsStr = "";
+  for (let l = (str.length - 1); l >= 0; l--) {
+    backwardsArr.push(str[l]);
+  }
+  while (backwardsArr.length !== 0) {
+    backwardsStr += backwardsArr.shift();
+  }
+  return backwardsStr
 };
 
 // Do not edit below this line
