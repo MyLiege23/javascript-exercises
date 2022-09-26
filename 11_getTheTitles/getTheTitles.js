032d035ab7cb35b4209b5b7c0dc34a94d7d9a302
MyLiege23 <hsabel@outlook.com>
2022-09-26 15:41:30 -0600

    Complete 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+  let titles = [];
+  for (let b = 0; b < arr.length; b++) {
+    titles.push(arr[b]["title"]);
+  }
+  return titles;
 };
+
+console.log(getTheTitles([
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]))
 
 // Do not edit below this line
 module.exports = getTheTitles;
