67358a61d40488bc4bc0ed1556dd3e8d2548ce21
MyLiege23 <hsabel@outlook.com>
2022-09-26 14:12:12 -0600

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n < 1) {
+    return "OOPS"
+  }
+  let fibRecentTwo = [0, 1];
+  for (l = 1; l < n; l++) {
+    let sum = fibRecentTwo[0] + fibRecentTwo[1];
+    fibRecentTwo.shift();
+    fibRecentTwo.push(sum);
+  }
+  return fibRecentTwo[1];
 };
 
 // Do not edit below this line
