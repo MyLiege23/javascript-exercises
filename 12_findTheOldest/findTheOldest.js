16ddb14b6462cc572e0b7ba0a6e0ae3e44c8618f
MyLiege23 <hsabel@outlook.com>
2022-09-26 16:45:56 -0600

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  //declare constant for the selection of the oldest person
+  let oldest = people[0];
 
+  //create a function to use to calculate the age of the person being compared to the oldest
+  let findAge = function(person) {
+    age = 0;
+    if (person.hasOwnProperty("yearOfDeath")) {
+      age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+      age = new Date().getFullYear() - person.yearOfBirth;
+    }
+    return age;
+  }
+
+  //compare person in question to the oldest and reassign oldest value it necessary
+  for (let c = 0; c < people.length; c++) {
+    if (findAge(people[c]) > findAge(oldest)) {
+      oldest = people[c];
+    }
+  }
+  return oldest;
 };
 
 // Do not edit below this line
